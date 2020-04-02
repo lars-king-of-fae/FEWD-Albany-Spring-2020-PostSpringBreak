51ea8f1083f41219208b6e05d6493f735f838ea4
lars-king-of-fae <ajmims16@gmail.com>
2020-04-02 17:09:30 -0400

    comm

---
+++ tuesday.js
@@ -0,0 +1,91 @@
+// var cake = "strawberry"
+
+// let upperCake = cake.toUpperCase()
+// console.log(upperCake)  
+// function singASong() {
+//     return "Staying Alive"
+// }
+// singASong();
+
+// function splitString (str) {
+//     console.log(str.split(""));
+//     return str.split("");
+// }
+
+// function add (num1, num2, num3) {
+//     return num1 + num2 + num3
+   
+// }
+
+// add()
+
+// function sub (a, b) {
+//     return a - b
+
+// }
+// console.log( add(34,5,3))
+// console.log(sub(100, 45));
+
+// let arr = ['mice', 'men', 'dogs']
+
+// function printWord(arr) {
+//     let arr = [];
+//     for(let 1 =0; 1 < pets.length; i++) {
+//         return arr[i];
+//     }
+
+// }
+
+// console.log(printWord(pets))
+
+// const person = {
+//     fName: "Nancy",
+//     lName: "Smith",
+//     hobbies: ['singing', 'running'],
+//     fullName [this.fName + this.lName,]
+//     greeting: function () {
+//         return " hello my name is " + this.fullName + " i enjoy " + this.hobbies[0] + " and " + this.hobbies[1]
+//     }
+// }
+// console.log(person,fName)
+// console.log(person.lName)
+
+// console.log(person.greeting())
+
+// let car = "dodge"
+// const pi =10;
+    
+// let name = "jay"
+// let age = 40;
+// let favMovie = "deadpool"
+// let intruments = ["piano", "bass"];
+
+// let sentence = "hello my name is " + " i am " + age +
+// " years young.  " +
+// favMovie +
+// " i love dogs. i play " + instruments[1] + "and " + instruments[0];
+// console.log(sentence)
+
+// let variable = 123
+
+
+// function doSomething() {
+//     ...
+// }
+
+
+// const books = {
+//     {
+//         title: "the cat in the hat",
+//         published:1960,
+//         coauthor: {
+//             name: 'Dr. Oz'
+//         },
+//         pages: 79};
+    
+
+//             console.log(books)
+
+
+
+
